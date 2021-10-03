ee351b6efb9ecd2c6118139365f5e9c78054a77e
Furqan Ali <87120247+hafizfurqan@users.noreply.github.com>
2021-10-03 13:48:37 +0500

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,10 @@
+vovels = 'a, e, i, o, u'
+consonants = 'Every alphabet not included in vovels'
+print ("vovels ="); print (vovels)
+print ("consonents ="); print (consonants)
+print("prints o") 
+print(vovels[9])
+print("prints till not")
+print(consonants[0:18])
+print('prints only not')
+print(consonants[15:18])
