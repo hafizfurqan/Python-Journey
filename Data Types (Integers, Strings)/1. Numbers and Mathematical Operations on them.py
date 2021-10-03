5992e539a1bcfb38c9241eb25b73c48f7ca5c5af
Furqan Ali <87120247+hafizfurqan@users.noreply.github.com>
2021-10-03 13:54:33 +0500

    Add files via upload

---
+++ Data Types (Integers, Strings)/1. Numbers and Mathematical Operations on them.py
@@ -0,0 +1,9 @@
+#Simple Mathematical Law values
+print("(30 * 50) * 5 / 2 + 7 - 25")
+print((30 * 50) * 5 / 2 + 7 - 25)
+#Simple power value
+print("23^2")
+print(23 ** 2)
+#Simple float value
+print(24 ** 2.0)
+print(24 ** 2.0)
---
+++ Data Types (Integers, Strings)/Variables.py
@@ -0,0 +1,4 @@
+fruit = "Guava"
+fortyfive = 45
+thirtyfive = 35
+print ("fruit price = "), print(fortyfive + thirtyfive)
