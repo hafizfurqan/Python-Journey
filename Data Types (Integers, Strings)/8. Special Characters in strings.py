27d7e51d752c48a9c1cdbad1ba7c123a22928e28
Furqan Ali <87120247+hafizfurqan@users.noreply.github.com>
2021-11-27 20:11:08 +0500

    Create 8. Special Characters in strings.py

---
+++ Data Types (Integers, Strings)/8. Special Characters in strings.py
@@ -0,0 +1,2 @@
+name = Apple \tGuava \nPineapple \rPomegranate #new line='\n', tab space='\t' reject words on same line='\r'[using between sentences will make sentence before it disappear upon print but still visible in code]
+print = name
