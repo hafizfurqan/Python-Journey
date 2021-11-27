05d988f0c662f8804c9f4c7d75f1ffd68e44d38a
Furqan Ali <87120247+hafizfurqan@users.noreply.github.com>
2021-11-27 20:12:44 +0500

    Update 7. Formatted f-String.py

--- Data Types (Integers, Strings)/7. Formatted f-String.py
+++ Data Types (Integers, Strings)/7. Formatted f-String.py
@@ -1,5 +1,5 @@
-1st = learning
-2nd = programming
+1st = 'learning'
+2nd = 'programming'
 3rd = 1st + ' ' + 2nd + '.'
 4th = f'{1st} {2nd}.'#This is called f string don't know, don't ask. 😋
 print (3rd)
