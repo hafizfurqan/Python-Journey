69e7c1b4e6c920b21eb445d6d204580e90d07da5
Furqan Ali <87120247+hafizfurqan@users.noreply.github.com>
2021-11-27 19:23:15 +0500

    Create 4. Functions on Strings.py

---
+++ Data Types (Integers, Strings)/4. Functions on Strings.py
@@ -0,0 +1,3 @@
+string = 'I am learning programming'
+print(string.uppercase())#>After string full stop(.) shows useful functions in IDE of your choice<
+print("If you place full stop after a string in print inside IDE, it'll show multiple functions which could be applied on it."())
