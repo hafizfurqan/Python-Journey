44ec4c8961846290727a0b281233f4e1770ebc64
Furqan Ali <87120247+hafizfurqan@users.noreply.github.com>
2021-11-27 19:49:59 +0500

    Create 5. Multiline Strings in Python.py

---
+++ Data Types (Integers, Strings)/5. Multiline Strings in Python.py
@@ -0,0 +1,17 @@
+Multiline-Message = '''
+I loved inventing things
+Them I wamted to learn old things to make new things
+During learning I got tired and rested a little
+I still am learning new, old and creative things &
+I am happy with learning programing.
+        
+            0     0
+        '             '
+         #           #
+           $ _____ $
+
+'''
+
+
+
+print (Multiline-Message)
