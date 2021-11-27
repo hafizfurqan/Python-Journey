15d8b6fa2d5813acf4771cd42ef5eb529c12b60d
Furqan Ali <87120247+hafizfurqan@users.noreply.github.com>
2021-11-27 19:30:30 +0500

    Update 4. Functions on Strings.py

--- Data Types (Integers, Strings)/4. Functions on Strings.py
+++ Data Types (Integers, Strings)/4. Functions on Strings.py
@@ -1,3 +1,4 @@
 string = 'I am learning programming'
 print(string.uppercase())#>After string full stop(.) shows useful functions in IDE of your choice<
+print(string.count('learning', 0, 15))#>This will tell whether or not there exist 'learning' in context til 15th word. Remember even a single space counts as a world in PL.
 print("If you place full stop after a string in print inside IDE, it'll show multiple functions which could be applied on it."())
